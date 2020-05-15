078cd49ff2d5d96f01c8c327129c4c25d15ff178
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-05-15 22:44:50 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,16 +1,25 @@
-def position_taken?(board, position)
-  if board[position] == " " || board[position] == "" || board[position] == nil
-  	false
-  else
-  	true
+def valid_move?(board, index)
+  def position_taken?(array, ind)
+    if array[ind] == " " || array[ind] == "" || array[ind] == nil
+      return false
+    else
+      return true
+    end
   end
-end
 
-def valid_move?(board, position)
-	spot = position.to_i - 1
-	if !position_taken?(board, spot) && spot.between?(0,8)
-		true
-	else
-		false
-	end
+  def on_board?(num)
+    if num.between?(0, 8) == true
+      return true
+    else
+      return false
+    end
+  end
+
+  if (position_taken?(board, index)) == false && (on_board?(index) == true)
+    return true
+  else
+    return false
+  end
+
+
 end
